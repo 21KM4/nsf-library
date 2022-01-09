9855bf66c294e5a2aee4276981712c1b18e52027
水本幸希 <48169975+21KM4@users.noreply.github.com>
2022-01-09 14:42:41 +0900

    Add files via upload

---
+++ gme/gme_types.h
@@ -0,0 +1,12 @@
+#ifndef GME_TYPES_H
+#define GME_TYPES_H
+
+/*
+ * This is a default gme_types.h for use when *not* using
+ * CMake.  If CMake is in use gme_types.h.in will be
+ * processed instead.
+ */
+#define USE_GME_NSF
+#define USE_GME_NSFE
+
+#endif /* GME_TYPES_H */
